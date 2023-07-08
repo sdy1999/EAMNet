94c30b3165029b1f02ffe7ff2cb66c1d9172c7d0
sdy1999 <44687858+sdy1999@users.noreply.github.com>
2023-07-08 12:04:38 +0800

    Create loss_function.py

---
+++ utils/loss_function.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn.functional as F
+
+
+# weighted cross-entropy loss function
+def wce_loss(pred, mask):
+    weit = 1 + 5 * torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
+    wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
+    wbce = (weit * wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
+
+    return wbce.mean()
+
+
+# weighted iou loss function
+def wiou_loss(pred, mask):
+    weit = 1 + 5 * torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
+    pred = torch.sigmoid(pred)
+    inter = ((pred * mask) * weit).sum(dim=(2, 3))
+    union = ((pred + mask) * weit).sum(dim=(2, 3))
+    wiou = 1 - (inter + 1) / (union - inter + 1)
+
+    return wiou.mean()
+
+
+def e_loss(pred, mask):
+    pred = torch.sigmoid(pred)
+    mpred = pred.mean(dim=(2, 3)).view(pred.shape[0], pred.shape[1], 1, 1).repeat(1, 1, pred.shape[2], pred.shape[3])
+    phiFM = pred - mpred
+
+    mmask = mask.mean(dim=(2, 3)).view(mask.shape[0], mask.shape[1], 1, 1).repeat(1, 1, mask.shape[2], mask.shape[3])
+    phiGT = mask - mmask
+
+    EFM = (2.0 * phiFM * phiGT + 1e-8) / (phiFM * phiFM + phiGT * phiGT + 1e-8)
+    QFM = (1 + EFM) * (1 + EFM) / 4.0
+    eloss = 1.0 - QFM.mean(dim=(2, 3))
+    return eloss.mean()
+
+
+def hybrid_e_loss(pred, mask):
+    """
+        SciChina-Hybrid Eloss-2020
+    :param pred:
+    :param mask:
+    :return:
+    """
+    # adaptive weighting masks
+    weit = 1 + 5 * torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
+
+    # weighted binary cross entropy loss function
+    wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
+    wbce = ((weit * wbce).sum(dim=(2, 3)) + 1e-8) / (weit.sum(dim=(2, 3)) + 1e-8)
+
+    # weighted e loss function
+    pred = torch.sigmoid(pred)
+    mpred = pred.mean(dim=(2, 3)).view(pred.shape[0], pred.shape[1], 1, 1).repeat(1, 1, pred.shape[2], pred.shape[3])
+    phiFM = pred - mpred
+
+    mmask = mask.mean(dim=(2, 3)).view(mask.shape[0], mask.shape[1], 1, 1).repeat(1, 1, mask.shape[2], mask.shape[3])
+    phiGT = mask - mmask
+
+    EFM = (2.0 * phiFM * phiGT + 1e-8) / (phiFM * phiFM + phiGT * phiGT + 1e-8)
+    QFM = (1 + EFM) * (1 + EFM) / 4.0
+    eloss = 1.0 - QFM.mean(dim=(2, 3))
+
+    # weighted iou loss function
+    inter = ((pred * mask) * weit).sum(dim=(2, 3))
+    union = ((pred + mask) * weit).sum(dim=(2, 3))
+    wiou = 1.0 - (inter + 1 + 1e-8) / (union - inter + 1 + 1e-8)
+
+    return (wbce + eloss + wiou).mean()
+    
+def structure_loss(pred, mask):
+    weit  = 1+5*torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15)-mask)
+    wbce  = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
+    wbce  = (weit*wbce).sum(dim=(2,3))/weit.sum(dim=(2,3))
+
+    pred  = torch.sigmoid(pred)
+    inter = ((pred*mask)*weit).sum(dim=(2,3))
+    union = ((pred+mask)*weit).sum(dim=(2,3))
+    wiou  = 1-(inter+1)/(union-inter+1)
+    return (wbce+wiou).mean()
+
+def CharbonnierLoss(pred,mask):
+    
+    eps = 1e-6
+    b, c, h, w = mask.size()
+    loss = torch.sum(torch.sqrt((pred - mask).pow(2) + eps**2))
+
+    return loss/(c*b*h*w)
